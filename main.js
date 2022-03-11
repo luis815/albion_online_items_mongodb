eff80e0804951e3a607711523d7c49f76f7be8f1
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-03-11 00:37:54 +0000

    Project config to store last meta version

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
+const fs = require("fs").promises;
+const path = require("path");
 const axios = require("axios").default;
 
 const main = async () => {
+	//Get albion online metadata version
 	const metaLatestCommitRes = await axios.get(
 		"https://api.github.com/repos/broderickhyman/ao-bin-dumps/commits/master",
 		{ responseType: "json" }
 	);
 
-    console.log(metaLatestCommitRes.data);
+	//Get local version
+	const projectConfig = JSON.parse(
+		await fs.readFile(path.resolve(__dirname, "./project-config.json"), "utf-8")
+	);
+
+	//Compare meta versions
+	if (metaLatestCommitRes.data.sha === projectConfig.currentMetaVersion) {
+		return;
+	}
+
+	//Since meta versions did not match, compile a new MongoDB collection
+	
+
+	//Update project-config.json
+	fs.writeFile(
+		path.resolve(__dirname, "./project-config.json"),
+		JSON.stringify({
+			currentMetaVersion: metaLatestCommitRes.data.sha,
+		}),
+		"utf-8"
+	);
 };
 
 main();
